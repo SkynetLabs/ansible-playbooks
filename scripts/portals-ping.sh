bff37fe14dcd921e25cfa26e1b2d1e41e1eec452
Filip Rysavy <fil@siasky.net>
2021-06-18 15:05:06 +0200

    Add root ssh check to portal setup

--- scripts/portals-ping.sh
+++ scripts/portals-ping.sh
@@ -2,6 +2,6 @@
 
 # Set command and arguments
 cmd=ansible
-args="--inventory /tmp/ansible-private/inventory/hosts.ini webportals -m ping $@ "
+args="--inventory /tmp/ansible-private/inventory/hosts.ini webportals -m ping -u user $@ "
 
 source $(dirname "$0")/lib/ansible-executor.sh
