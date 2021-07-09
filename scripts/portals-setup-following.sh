60dab805141ad8623934788c5870acf12384b839
Filip Rysavy <fil@siasky.net>
2021-07-09 17:46:24 +0200

    Handle blockchain sysncing and wallet initialization gracefully

--- scripts/portals-setup-following.sh
+++ scripts/portals-setup-following.sh
@@ -2,7 +2,7 @@
 
 # Set command and arguments
 cmd=ansible-playbook
-args="--inventory /tmp/ansible-private/inventory/hosts.ini playbooks/portals-setup.yml $@"
+args="--inventory /tmp/ansible-private/inventory/hosts.ini playbooks/portals-setup-following.yml $@"
 
 # Execute the command
 source $(dirname "$0")/lib/ansible-executor.sh
