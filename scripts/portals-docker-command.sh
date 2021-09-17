4dcae937c3758036500308d0a5d38fa5ab74af1a
Matthew Sevey <mjsevey@gmail.com>
2021-09-17 10:53:00 -0400

    Update to handle multiple docker commands and if the variable is set or not

--- scripts/portals-docker-command.sh
+++ scripts/portals-docker-command.sh
@@ -2,7 +2,7 @@
 
 # Set command and arguments
 cmd=ansible-playbook
-args="--inventory /tmp/ansible-private/inventory/hosts.ini playbooks/x-portals-docker-command.yml $@"
+args="--inventory /tmp/ansible-private/inventory/hosts.ini playbooks/portals-docker-command.yml $@"
 
 # Execute the command
 source $(dirname "$0")/lib/ansible-executor.sh
