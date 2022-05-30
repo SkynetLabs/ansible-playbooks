cef927014183e0a5a75f3cad4c2db3501c7430f8
Filip Rysavy <29089732+firyx@users.noreply.github.com>
2022-05-30 12:11:23 +0200

    Add support for HashiCorp Vault login

--- scripts/lib/hashicorp-vault-login-subscript.sh
+++ scripts/lib/hashicorp-vault-login-subscript.sh
@@ -1,14 +1,16 @@
+# Handle arguments:
 hcv_username="$1"
 hcv_password="$2"
 
-# Set working directory to ansible-playbooks
+# Set working directory to ansible-playbooks root directory:
 script_path="$BASH_SOURCE"
 script_dir=$(dirname "$script_path")
 ansible_playbooks_dir="$script_dir/../.."
 pushd $ansible_playbooks_dir > /dev/null
 
-# Try to login to HashiCorp Vault.
-# Use host network so that 127.0.0.1 is not inside curl container but on host.
+# Try to login to HashiCorp Vault:
+# Use host network so that possible Vault server addresss at 127.0.0.1 is not
+# inside curl container but on host.
 login_result=$(docker run --rm --network host dwdraju/alpine-curl-jq \
   curl \
   --request POST \
@@ -16,7 +18,7 @@ login_result=$(docker run --rm --network host dwdraju/alpine-curl-jq \
   --data '{"password": "'"$hcv_password"'"}' \
   "$HCV_URL/v1/auth/userpass/login/$hcv_username")
 
-# Exit if there was a docker run or curl error.
+# Exit if there was a docker run or curl error:
 ec=$?
 if [[ $ec -ne 0 ]]; then
   echo "There was a docker run curl error."
@@ -24,10 +26,11 @@ if [[ $ec -ne 0 ]]; then
   exit $ec
 fi
 
-# Exit if there was Vault error (Vault is sealed, login not successfull, ...).
+# Exit if there was Vault error (Vault is sealed, login not successfull, ...):
 if [[ "$login_result" == *"errors"* ]]; then
   echo "$login_result"
 
+  # Log extra message if Vault is sealed:
   if [[ "$login_result" == *"Vault is sealed"* ]]; then
     echo "    Vault is sealed e.g. after the vault container restart or the server restart."
     echo "    You need to unseal the Vault via web UI or command line."
@@ -37,11 +40,11 @@ if [[ "$login_result" == *"errors"* ]]; then
   exit 111
 fi
 
-# Extract HashiCorp Vault client token from response data.
+# Extract HashiCorp Vault client token from response data:
 # -r switch is to remove quotes from the result.
 token_result=$(echo "$login_result" | docker run -i --rm dwdraju/alpine-curl-jq jq -r .auth.client_token)
 
-# Exit if there was a docker or jq error.
+# Exit if there was a docker or jq error:
 ec=$?
 if [[ $ec -ne 0 ]]; then
   echo "There was a docker run jq error."
@@ -49,12 +52,13 @@ if [[ $ec -ne 0 ]]; then
   exit $ec
 fi
 
-# Exit if token was not found.
+# Exit if token was not found:
 if [[ "$token_result" == "null" ]]; then
   echo "Token couldn't be found using jq."
   popd > /dev/null
   exit 111
 fi
 
+# Return result (client token):
 echo "hcv_token: $token_result"
 popd > /dev/null
