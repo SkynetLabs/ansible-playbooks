8081f37e4fe8396ba9ca4b3cb9712b52c1fe53ee
Karol Wypchlo <kwypchlo@gmail.com>
2022-08-01 16:38:59 +0200

    remove debug script and playbook

--- scripts/portals-versions-set.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Set command and arguments
-cmd=ansible-playbook
-args="--inventory /tmp/ansible-private/inventory/hosts.ini playbooks/portals-versions-set.yml $@"
-
-# Execute the command
-source $(dirname "$0")/lib/ansible-executor.sh
