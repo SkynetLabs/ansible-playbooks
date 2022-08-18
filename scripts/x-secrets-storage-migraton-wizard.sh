0a61633629e450d6b09baab441a697b655826a8a
Filip Rysavy <29089732+firyx@users.noreply.github.com>
2022-08-18 14:41:22 +0200

    Add checking for cluster config lists

--- scripts/x-secrets-storage-migraton-wizard.sh
+++ scripts/x-secrets-storage-migraton-wizard.sh
@@ -2,7 +2,7 @@
 
 # Set command and arguments
 cmd=ansible-playbook
-args="--inventory /tmp/ansible-private/inventory/hosts.ini playbooks/x-secrets-storages-migraton-wizard.yml $@"
+args="--inventory /tmp/ansible-private/inventory/hosts.ini playbooks/x-secrets-storage-migraton-wizard.yml $@"
 
 # Execute the command
 source $(dirname "$0")/lib/ansible-executor.sh
